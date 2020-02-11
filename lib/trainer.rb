c6b30f05ce5f1812425070428a6a58418bb7fd6c
Agandaur <cjwagstaffe@gmail.com>
2020-02-11 09:17:34 -0800

    Added the pick_pokemon method to Trainer class

--- lib/trainer.rb
+++ lib/trainer.rb
@@ -1,4 +1,17 @@
 class Trainer < ActiveRecord::Base
     has_many :pokemons
     has_many :battles, through: :pokemons
+
+    def pick_pokemon
+        remove_id_if_found = Pokemon.all.find{|p| p.trainer_id == self.id}
+        if remove_id_if_found != nil
+            remove_id_if_found.trainer_id = nil
+            remove_id_if_found.save
+        end
+        available_list = Pokemon.all.select{|p| p.trainer_id == nil}
+        choice = available_list.sample
+        choice.trainer_id = self.id
+        choice.save
+        choice
+    end
 end
