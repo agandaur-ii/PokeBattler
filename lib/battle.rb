a040dadc85a28e7b5acc803878f765e68bb7b5bf
Agandaur <cjwagstaffe@gmail.com>
2020-02-11 13:32:45 -0800

    First shot at battle system

--- lib/battle.rb
+++ lib/battle.rb
@@ -7,12 +7,12 @@ class Battle < ActiveRecord::Base
         @player_two = nil
         list = [@pokemon_1, @pokemon_2]
 
-        if @pokemon_1.speed == @pokemon_2.speed
+        if @pokemon_1_temp_speed == @pokemon_2_temp_speed
             @player_one = list.delete(list.sample)
             @player_two = list[0]
         end
 
-        if @pokemon_1.speed > @pokemon_2.speed
+        if @pokemon_1_temp_speed > @pokemon_2_temp_speed
             @player_one = @pokemon_1
             @player_two = @pokemon_2
         else
@@ -21,27 +21,31 @@ class Battle < ActiveRecord::Base
         end
     end
 
-    def attack
-
+    def attack(player_num)
+        if player_num == @pokemon_1
+            @pokemon_2_temp_hp -= @pokemon_1_temp_attack
+        else
+            @pokemon_1_temp_hp -= @pokemon_2_temp_attack
+        end
     end
 
-    def boost
+    def boost(player_num)
         puts "Your pokemon feels stronger!"
     end
 
-    def turn(player)
+    def turn(player_num)
         #attack or boost?
     end
 
     def start
-        pokemon_1_temp_hp = @pokemon_1.hp
-        #pokemon_1_temp_attack = @pokemon_1.attack
-        #pokemon_1_temp_speed = @pokemon_1.speed
-        pokemon_2_temp_hp = @pokemon_2.hp
-        #pokemon_2_temp_attack = @pokemon_2.attack
-        #pokemon_2_temp_speed = @pokemon_2.speed
+        @pokemon_1_temp_hp = @pokemon_1.hp
+        @pokemon_1_temp_attack = @pokemon_1.attack
+        @pokemon_1_temp_speed = @pokemon_1.speed
+        @pokemon_2_temp_hp = @pokemon_2.hp
+        @pokemon_2_temp_attack = @pokemon_2.attack
+        @pokemon_2_temp_speed = @pokemon_2.speed
 
-        until pokemon_1_temp_hp == 0 || pokemon_2_temp_hp == 0 do 
+        until @pokemon_1_temp_hp == 0 || @pokemon_2_temp_hp == 0 do 
           who_goes_first?
           turn(@player_one)
           turn(@player_two)
