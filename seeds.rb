41cf3470ea9f19c6c506cf48c92df4efcb1469ac
Agandaur <cjwagstaffe@gmail.com>
2020-02-10 17:50:49 -0800

    Added stats to seed Pokemon

--- seeds.rb
+++ seeds.rb
@@ -8,13 +8,16 @@ red = Trainer.new(name: "Red")
 blue = Trainer.new(name: "Blue")
 
 
-eevee = Pokemon.new(name: "Eevee", hp: , attack: , speed: )
-flareon = Pokemon.new(name: "Flareon", hp: , attack: , speed: )
-jolteon = Pokemon.new(name: "Jolteon", hp: , attack: , speed: )
-vaporeon = Pokemon.new(name: "Vaporeon", hp: , attack: , speed: )
-espeon = Pokemon.new(name: "Espeon", hp: , attack: , speed: )
-umbreon = Pokemon.new(name: "Umbreon", hp: , attack: , speed: )
-glaceon = Pokemon.new(name: "Glaceon", hp: , attack: , speed: )
-leafeon = Pokemon.new(name: "Leafeon", hp: , attack: , speed: )
-sylveon = Pokemon.new(name: "Sylveon", hp: , attack: , speed: )
+#HP = HP + Def + SpDef
+#Attack = (Attack + SpAtt) / 2
+#Speed = Speed
+eevee = Pokemon.new(name: "Eevee", hp: 170, attack: 50, speed: 55)
+flareon = Pokemon.new(name: "Flareon", hp: 235, attack: 56, speed: 65)
+jolteon = Pokemon.new(name: "Jolteon", hp: 220, attack: 44, speed: 130)
+vaporeon = Pokemon.new(name: "Vaporeon", hp: 285, attack: 44, speed: 65)
+espeon = Pokemon.new(name: "Espeon", hp: 220, attack: 49, speed: 110)
+umbreon = Pokemon.new(name: "Umbreon", hp: 335, attack: 31, speed: 65)
+glaceon = Pokemon.new(name: "Glaceon", hp: 270, attack: 48, speed: 65)
+leafeon = Pokemon.new(name: "Leafeon", hp: 260, attack: 43, speed: 95)
+sylveon = Pokemon.new(name: "Sylveon", hp: 290, attack: 44, speed: 60)
 
