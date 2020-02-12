394199c94facaf83e33d07e02dfeab70101740b2
Agandaur <cjwagstaffe@gmail.com>
2020-02-11 23:31:07 -0800

    battle! now removes id from rival and user pokemon

--- lib/trainer.rb
+++ lib/trainer.rb
@@ -55,6 +55,9 @@ class Trainer < ActiveRecord::Base
         total = []
         total << win_list
         total << loss_list
+        if total.length == 0
+            return "You haven't used any Pokemon yet."
+        end
         total.flatten.uniq
     end
 
@@ -66,11 +69,17 @@ class Trainer < ActiveRecord::Base
         total = []
         total << win_list
         total << loss_list
+        if total.length == 0
+            return "You haven't battled yet."
+        end
         total.flatten.uniq
     end
 
     def arch_rival
         #trainer you have lost to the most
+        if loss_instances.length == 0
+            return "You have no rival yet!"
+        end
         loss_list_ids = loss_instances.map{|l| l.winning_trainer_id}
         loss_list = loss_list_ids.map{|l| Trainer.find(l)}.map{|trainer| trainer.name}
         total = []
@@ -85,10 +94,8 @@ class Trainer < ActiveRecord::Base
             end
         end
 
-        if rivals.keys.length == nil
-            puts "You have no rival yet!"
-        elsif rivals.keys.length == 1
-            puts "Your rival is #{rivals[0]}"
+        if rivals.keys.length == 1
+            puts "Your rival is #{rivals.keys[0]}"
         else
             puts "You have many rivals!"
             rivals.keys.each{|rival| puts "#{rival}"}
@@ -97,10 +104,56 @@ class Trainer < ActiveRecord::Base
 
     def fav_pokemon
         #pokemon that has gotten you the most wins
+        if win_instances.length == 0
+            return "You don't have a favorite Pokemon yet."
+        end
+        win_list_ids = win_instances.map{|w| w.winning_pokemon_id}
+        win_list = win_list_ids.map{|w| Pokemon.find(w)}.map{|pokemon| pokemon.name}
+        total = []
+        total << win_list
+        flat = total.flatten
+        pokemon_count = Hash.new(0)
+        flat.each {|pokemon_name| pokemon_count[pokemon_name] += 1}
+        max_value = pokemon_count.map{|k,v| v}.sort.last
+        fav = pokemon_count.select do |k,v|
+            if v == max_value
+                k
+            end
+        end
+
+        if fav.keys.length == 1
+            puts "#{fav.keys[0]} is your favorite Pokemon!"
+        else
+            puts "All of these Pokemon are your favorite!"
+            fav.keys.each{|fav| puts "#{fav}"}
+        end
     end
 
     def not_you_again
         #pokemon you have lost to the most
+        if loss_instances.length == 0
+            return "No Pokemon has beaten you yet!"
+        end
+        loss_list_ids = loss_instances.map{|l| l.winning_pokemon_id}
+        loss_list = loss_list_ids.map{|l| Pokemon.find(l)}.map{|pokemon| pokemon.name}
+        total = []
+        total << loss_list
+        flat = total.flatten
+        pokemon_count = Hash.new(0)
+        flat.each {|pokemon_name| pokemon_count[pokemon_name] += 1}
+        max_value = pokemon_count.map{|k,v| v}.sort.last
+        rivals = pokemon_count.select do |k,v|
+            if v == max_value
+                k
+            end
+        end
+
+        if rivals.keys.length == 1
+            puts "#{rivals.keys[0]} has beaten you the most"
+        else
+            puts "All these Pokemon have given you the beat down!"
+            rivals.keys.each{|rival| puts "#{rival}"}
+        end
     end
 
     def battle!
@@ -116,10 +169,56 @@ class Trainer < ActiveRecord::Base
         battle.t_id_two = rival_pokemon.trainer_id
         result = battle.start
         rival_pokemon.update(trainer_id: nil)
+        user_pokemon.update(trainer_id: nil)
+        user_pokemon.trainer_id = nil
         if result == self.id
             puts "You won!"
         else
             puts "You lost."
+        end
+    end
+
+    def self.the_best
+        win_list_ids = Battle.all.map{|w| w.winning_trainer_id}
+        winning_trainer_list = win_list_ids.map{|w| Trainer.find(w)}.map{|trainer| trainer.name}
+        trainer_count = Hash.new(0)
+        winning_trainer_list.each {|trainer_name| trainer_count[trainer_name] += 1}
+        max_value = trainer_count.map{|k,v| v}.sort.last
+        best = trainer_count.select do |k,v|
+            if v == max_value
+                k
+            end
+        end
+
+        if best.keys.length == 0
+            puts "There is no best trainer yet"
+        elsif best.keys.length == 1
+            puts "#{best.keys[0]} is the best trainer!"
+        else
+            puts "These are all the best trainers:"
+            best.keys.each{|b| puts "#{b}"}
+        end
+    end
+
+    def self.the_worst
+        loss_list_ids = Battle.all.map{|l| l.losing_trainer_id}
+        losing_trainer_list = loss_list_ids.map{|l| Trainer.find(l)}.map{|trainer| trainer.name}
+        trainer_count = Hash.new(0)
+        losing_trainer_list.each {|trainer_name| trainer_count[trainer_name] += 1}
+        lowest_value = trainer_count.map{|k,v| v}.sort.first
+        worst = trainer_count.select do |k,v|
+            if v == lowest_value
+                k
+            end
+        end
+
+        if worst.keys.length == 0
+            puts "There is no worst trainer yet"
+        elsif worst.keys.length == 1
+            puts "#{worst.keys[0]} is the worst trainer...."
+        else
+            puts "These are all the worst trainers:"
+            worst.keys.each{|w| puts "#{w}"}
         end
     end
 
